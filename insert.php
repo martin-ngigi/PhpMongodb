76c6008ba9714a3c61f2985b7a2cbcab96c1589b
martin-ngigi <martinwainaina001@gmail.com>
2022-10-15 11:31:59 +0300

    Connecting to online Atlass success ;-)

--- insert.php
+++ insert.php
@@ -8,19 +8,37 @@
 
 	require_once __DIR__ .'/vendor/autoload.php';
 	
-	//require __DIR__.'../../vendor.autoload.php';
-	$con = new MongoDB\Client("mongodb://localhost:27017");
+	// //require __DIR__.'../../vendor.autoload.php';
+	// $con = new MongoDB\Client("mongodb://localhost:27017");
+	// print_r($con);
+
+	// //create a database automatically called database2022
+	// $db = $con -> database2022;
+
+
+	// //create a table automatically called table2022
+	// $tbl = $db -> table2022;
+
+	// //after creating, it will not reflect in compass untill one inserts
+
+	// //inserting 
+	// $tbl -> insertOne(["Name" => "Ken", "Year" => 2021]);
+
+		//require __DIR__.'../../vendor.autoload.php';
+	$con = new MongoDB\Client(
+		'mongodb+srv://wainaina:1234@cluster0.5sgivs4.mongodb.net/PhpDb?serverSelectionTryOnce=false&serverSelectionTimeoutMS=150000&w=majority'
+	);
 	print_r($con);
 
-	//create a database automatically called database2022
-	$db = $con -> database2022;
+	//create a database  called PhpDb
+	$db = $con->PhpDb;
 
 
-	//create a table automatically called table2022
-	$tbl = $db -> table2022;
+	//create a table automatically called PhpCollection
+	$tbl = $db->PhpCollection;
 
 	//after creating, it will not reflect in compass untill one inserts
 
 	//inserting 
-	$tbl -> insertOne(["Name" => "Wainaina", "Year" => 2022]);
+	$tbl -> insertOne(["Name"=>"Martin", "Year"=>2022])
 ?>
